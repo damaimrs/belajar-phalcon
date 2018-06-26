85568482e97f7ca11e65f81b3b09a24abefe20e1
ikrom18 <ichrome.fahdi@gmail.com>
2018-06-26 14:25:28 +0700

    fix error return after action

--- app/controllers/DataController.php
+++ app/controllers/DataController.php
@@ -1,105 +1,101 @@
 <?php
 
-class DataController extends ControllerBase
-{
+class DataController extends ControllerBase {
 
-    public function indexAction()
-    {
-    	$pegawai = Pegawai::find();
-    	$this->view->pegawai = $pegawai;
-    }
+	public function indexAction() {
+		$pegawai = Pegawai::find();
+		$this->view->pegawai = $pegawai;
+	}
 
-    public function createAction()
-    {
-    	$pegawai = new Pegawai();
-    	$pegawai->nama_pegawai = $this->request->getPost("nama-pegawai");
-    	$pegawai->email_pegawai = $this->request->getPost("email-pegawai");
+	public function createAction() {
+		$pegawai = new Pegawai();
+		$pegawai->nama_pegawai = $this->request->getPost("nama-pegawai");
+		$pegawai->email_pegawai = $this->request->getPost("email-pegawai");
 
-    	if (!$pegawai->save()) {
+		if (!$pegawai->save()) {
 
-    		$this->flash->danger('Gagal Menambah Pegawai');
+			$this->flash->danger('Gagal Menambah Pegawai');
+			return $this->response->redirect('data/index');
+			// Forward to the index action
+			// return $this->dispatcher->forward(
+			// 	[
+			// 		'action' => 'index',
+			// 	]
+			// );
 
-    		// Forward to the index action
-    		return $this->dispatcher->forward(
-	            [
-	                'action' => 'index'
-	            ]
-        	);
-
-        	// Make a full HTTP redirection
-        	// return $this->response->redirect('contact/index');
-   			// $message = "Gagal Menambah Pegawai!";
+			// Make a full HTTP redirection
+			// return $this->response->redirect('contact/index');
+			// $message = "Gagal Menambah Pegawai!";
 			// echo "<script type='text/javascript'>alert('$message');</script>";
-    	}
-    	else {
+		} else {
 
-    		$this->flash->success('Berhasil Menambah Pegawai');
+			$this->flash->success('Berhasil Menambah Pegawai');
+			return $this->response->redirect('data/index');
+			// Forward to the index action
+			// return $this->dispatcher->forward(
+			//        [
+			//            'action' => 'index'
+			//        ]
+			//   	);
 
-    		// Forward to the index action
-    		return $this->dispatcher->forward(
-	            [
-	                'action' => 'index'
-	            ]
-        	);
-
-        	// Make a full HTTP redirection
-        	// return $this->response->redirect('contact/index');
-   			//$message = "Berhasil Menambah Pegawai!";
+			// Make a full HTTP redirection
+			// return $this->response->redirect('contact/index');
+			//$message = "Berhasil Menambah Pegawai!";
 			// echo "<script type='text/javascript'>alert('$message');</script>";
-    	}    	
-    }
+		}
+	}
 
-    public function updateAction($id) {
-    	
-    	$pegawai = Pegawai::findFirstByidPegawai($id);
-    	$pegawai->nama_pegawai = $this->request->getPost("nama-pegawai");
-    	$pegawai->email_pegawai = $this->request->getPost("email-pegawai");
+	public function updateAction($id) {
 
-    	if(!$pegawai->save()){
+		$pegawai = Pegawai::findFirstByidPegawai($id);
+		$pegawai->nama_pegawai = $this->request->getPost("nama-pegawai");
+		$pegawai->email_pegawai = $this->request->getPost("email-pegawai");
 
-   			return $this->dispatcher->forward(
-                [
-                    'action' => 'index'
-                ]
-            );
+		if (!$pegawai->save()) {
+			return $this->response->redirect('data/index');
+			// return $this->dispatcher->forward(
+			// 	[
+			// 		'action' => 'index',
+			// 	]
+			// );
 
-    	} else {
+		} else {
+			return $this->response->redirect('data/index');
+			// return $this->dispatcher->forward(
+			// 	[
+			// 		'action' => 'index',
+			// 	]
+			// );
 
-    		return $this->dispatcher->forward(
-                [
-                    'action' => 'index'
-                ]
-            );
+		}
+	}
 
-    	}
-    }
+	public function deleteAction($id) {
 
-    public function deleteAction($id) {
+		$pegawai = Pegawai::findFirstByidPegawai($id);
 
-        $pegawai = Pegawai::findFirstByidPegawai($id);
+		if (!$pegawai->delete()) {
 
-        if(!$pegawai->delete()){
+			$message = "Gagal Menghapus Pegawai!";
+			echo "<script type='text/javascript'>alert('$message');</script>";
+			return $this->response->redirect('data/index');
+			// return $this->dispatcher->forward(
+			// 	[
+			// 		'action' => 'index',
+			// 	]
+			// );
 
-            $message = "Gagal Menghapus Pegawai!";
-            echo "<script type='text/javascript'>alert('$message');</script>";
-            return $this->dispatcher->forward(
-                [
-                    'action' => 'index'
-                ]
-            );
+		} else {
+			$message = "Berhasil Menghapus Pegawai!";
+			echo "<script type='text/javascript'>alert('$message');</script>";
+			return $this->response->redirect('data/index');
+			// return $this->dispatcher->forward(
+			// 	[
+			// 		'action' => 'index',
+			// 	]
+			// );
+		}
 
-        } else {
-            $message = "Berhasil Menghapus Pegawai!";
-            echo "<script type='text/javascript'>alert('$message');</script>";
-            return $this->dispatcher->forward(
-                [
-                    'action' => 'index'
-                ]
-            );
-        }
-
-    }
-
+	}
 
 }
-
